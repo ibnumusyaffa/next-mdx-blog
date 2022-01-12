55e143c7417284944081cf751c284ba56933ee7b
ibnu_musyaffa <ibnu@badr-interactive.com>
2022-01-12 20:39:41 +0700

    refactor

--- components/Layout.js
+++ components/Layout.js
@@ -1,72 +1,37 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Meta from "../components/Meta";
-import MenuItem from "../components/MenuItem";
-import MenuItemMobile from "../components/MenuItemMobile";
-import MenuIcon from "../components/icons/Menu";
-import CloseIcon from "../components/icons/Close";
-import clsx from "clsx";
+
 
 function Layout({ children, meta }) {
-  let [show, setShow] = useState(false);
-
-  function toggle() {
-    setShow((prev) => !prev);
-  }
-
-  let navClass = clsx(" h-16 flex justify-center", {
-    "border-b border-gray-200 ": !show,
-  });
   return (
     <React.Fragment>
       <Meta {...meta}></Meta>
       <nav>
-        <div className="bg-gradient-to-r from-green-500 via-sky-400 to-pink-400 h-0.5"></div>
-        <div className={navClass}>
-          <div className="px-5 md:px-0 w-full md:w-1/2   h-full flex items-center justify-between">
+        <div className="h-20 flex justify-center border-b">
+          <div className="px-5 md:px-0 w-full md:w-5/12   h-full flex items-center justify-between">
             <Link href="/">
-              <a className="flex items-center space-x-3 hover:underline">
-                <div className="w-10 h-10 rounded-full bg-gradient-to-r from-green-400 to-blue-500"></div>
-                <div className=" text-gray-700">Ibnu Musyaffa</div>
+              <a className="flex items-center space-x-3 ">
+                <div className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-600 to-teal-500">
+                 Ibnu.dev
+                </div>
               </a>
             </Link>
-
-            <div className="hidden md:flex h-full">
-              <MenuItem href="/article">Artikel</MenuItem>
-              <MenuItem href="/tips">ProTips</MenuItem>
-            </div>
-            {show ? (
-              <button className="md:hidden" onClick={toggle}>
-                <CloseIcon></CloseIcon>
-              </button>
-            ) : (
-              <button className="md:hidden" onClick={toggle}>
-                <MenuIcon></MenuIcon>
-              </button>
-            )}
           </div>
         </div>
-        {show ? (
-          <div className="relative z-10">
-            <div className="border-b flex flex-col shadow-sm absolute w-full bg-white">
-              <MenuItemMobile href="/article">Artikel</MenuItemMobile>
-              <MenuItemMobile href="/tips">ProTips</MenuItemMobile>
-            </div>
-          </div>
-        ) : null}
       </nav>
 
       <div
-        className="flex justify-center py-5"
+        className="flex justify-center py-5 mt-5"
         style={{
           minHeight: "calc(100vh - 8rem)",
         }}
       >
-        <main className="px-5 md:px-0 w-full md:w-1/2">{children}</main>
+        <main className="px-5 md:px-0 w-full md:w-5/12">{children}</main>
       </div>
       <footer className="flex justify-center">
         <div className="px-5 md:px-0 w-full md:w-1/2 text-gray-600 text-sm border-t border-gray-300 py-5 text-center">
-          © 2021. Ibnu Musyaffa
+          © 2022. Ibnu Musyaffa
         </div>
       </footer>
     </React.Fragment>
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
 import React from "react";
 import Layout from "../components/Layout";
-function index() {
+import Link from "next/link";
+
+import Tag from "../components/Tag";
+import { getAllPosts } from "../helpers/MDXHelper";
+
+import path from "path";
+function Index({ posts }) {
   return (
     <Layout>
-      <div className="flex h-64  justify-center flex-col">
-        <div>
-          <span className="text-2xl md:text-3xl pb-2 font-semibold text-gray-800">
-            Halo, Saya Ibnu Musyaffa 👋
-          </span>
-          <div className="mt-3 text-gray-700">
-            Saya adalah seorang software developer, dan ini adalah blog pribadi
-            yang berisi tulisan seputar pengembangan perangkat lunak
-          </div>
-        </div>
+      <div className="flex flex-col space-y-8">
+        {posts.map((item) => (
+          <Link key={item.id} href={`/article/${item.slug}`}>
+            <a className="group space-y-2 ">
+              <div className="text-2xl font-bold text-gray-900 hover:underline">
+                {item.title}
+              </div>
+              <div className="flex items-center space-x-3">
+                <div className="text-sm text-gray-700">{item.date}</div>
+                <div className="text-gray-700">·</div>
+                <div className="text-sm text-gray-700">{item.readingTime}</div>
+                <div className="text-gray-700">·</div>
+                <div>
+                  <Tag variant={item.category_color}>{item.category}</Tag>
+                </div>
+              </div>
+
+              <div className="text-sm text-gray-700">{item.description}</div>
+            </a>
+          </Link>
+        ))}
       </div>
     </Layout>
   );
 }
 
-export default index;
+export async function getStaticProps() {
+  const POSTS_PATH = path.join(process.cwd(), "content", "articles");
+  let posts = await getAllPosts(POSTS_PATH);
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Index;
