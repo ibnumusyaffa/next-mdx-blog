98e983a521b2fb429f25a73a68da1d77820ebb4d
ibnu_musyaffa <ibnu@badr-interactive.com>
2022-01-13 21:15:29 +0700

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ function Layout({ children, meta }) {
       <Meta {...meta}></Meta>
       <nav>
         <div className="h-20 flex justify-center border-b">
-          <div className="px-5 md:px-0 w-full md:w-5/12   h-full flex items-center justify-between">
+          <div className="px-5 md:px-0 w-full md:w-[45%]   h-full flex items-center justify-between">
             <Link href="/">
               <a className="flex items-center space-x-3 ">
                 <div className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-600 to-teal-500">
@@ -27,10 +27,10 @@ function Layout({ children, meta }) {
           minHeight: "calc(100vh - 8rem)",
         }}
       >
-        <main className="px-5 md:px-0 w-full md:w-5/12">{children}</main>
+        <main className="px-5 md:px-0 w-full md:w-[45%]">{children}</main>
       </div>
       <footer className="flex justify-center">
-        <div className="px-5 md:px-0 w-full md:w-1/2 text-gray-600 text-sm border-t border-gray-300 py-5 text-center">
+        <div className="px-5 md:px-0 w-full md:w-[45%] text-gray-600 text-sm border-t border-gray-300 py-5 text-center">
           © 2022. Ibnu Musyaffa
         </div>
       </footer>
