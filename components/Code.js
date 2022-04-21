34921df2f96aa9e48a8bf3328392665b2b2bc584
ibnu_musyaffa <ibnu@badr-interactive.com>
2022-04-21 22:27:49 +0700

    update

--- components/Code.js
+++ components/Code.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import theme from "prism-react-renderer/themes/nightOwl";
+import theme from "prism-react-renderer/themes/dracula";
 import rangeParser from "parse-numeric-range";
 
 const calculateLinesToHighlight = (meta) => {
@@ -26,43 +26,60 @@ function Code({ children, className, ...other }) {
   const shouldHighlightLine = calculateLinesToHighlight(other.line);
 
   return (
-    <Highlight
-      {...defaultProps}
-      code={children}
-      theme={theme}
-      language={language}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={`${className} overflow-x-auto p-3  -mx-5 md:mx-0 md:rounded`}
-          style={style}
+    <React.Fragment>
+      {other.filename ? (
+        <div
+          style={{
+            backgroundColor: "rgb(40, 42, 54)",
+          }}
+          className="px-3 rounded-t-md text-sm py-2 border-b border-gray-700 text-gray-300"
         >
-          {tokens.map((line, i) => {
-            if (tokens.length - 1 == i) {
-              return null;
-            }
+          File : {other.filename}
+        </div>
+      ) : null}
 
-            let highLighClass = shouldHighlightLine(i) ? "highlight-line" : null;
-            return (
-              <div
-                key={i}
-                {...getLineProps({ line, key: i })}
-                className="table-row"
-              >
-                <span className="table-cell text-sm text-right select-none opacity-50 pr-5">
-                  {i + 1}
-                </span>
-                <span className={`table-cell text-sm ${highLighClass}`}>
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </span>
-              </div>
-            );
-          })}
-        </pre>
-      )}
-    </Highlight>
+      <Highlight
+        {...defaultProps}
+        code={children}
+        theme={theme}
+        language={language}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={`${className} overflow-x-auto p-3  -mx-5 md:mx-0 md:rounded-b-md ${
+              other.filename ? "" : "md:rounded-t-md"
+            }`}
+            style={style}
+          >
+            {tokens.map((line, i) => {
+              if (tokens.length - 1 == i) {
+                return null;
+              }
+
+              let highLighClass = shouldHighlightLine(i)
+                ? "highlight-line"
+                : null;
+              return (
+                <div
+                  key={i}
+                  {...getLineProps({ line, key: i })}
+                  className={`table-row ${highLighClass}`}
+                >
+                  <span className="table-cell text-sm text-right select-none opacity-50 pr-5">
+                    {i + 1}
+                  </span>
+                  <span className={`table-cell text-sm`}>
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </span>
+                </div>
+              );
+            })}
+          </pre>
+        )}
+      </Highlight>
+    </React.Fragment>
   );
 }
 
