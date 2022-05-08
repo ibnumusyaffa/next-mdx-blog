431741ad260624935f0ff2dce0a64b855df174af
ibnu_musyaffa <ibnu@badr-interactive.com>
2022-05-08 12:03:19 +0700

    update font

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
-import "@fontsource/roboto"; 
-import "@fontsource/jetbrains-mono";
+
+import "@fontsource/roboto/400.css"; 
+import "@fontsource/roboto/500.css"; 
+import "@fontsource/roboto/700.css"; 
+import "@fontsource/roboto/900.css"; 
+import "@fontsource/roboto/400-italic.css"; 
+import "@fontsource/roboto/500-italic.css"; 
+
+
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/400-italic.css";
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
