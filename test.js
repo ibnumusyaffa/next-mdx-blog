15bba29f167686586492b6c148633339ce71bd6a
ibnu_musyaffa <ibnu@badr-interactive.com>
2022-05-28 23:22:30 +0700

    update

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-let data = ["dog", "bird", "rabbit", "cat"];
+function binarySearch(data, x) {
+  let start = 0;
+  let end = data.length - 1;
 
-function findCat(data) {
-  for (const item of data) {
-    if (item === "cat") {
-      return item;
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    console.log(middle)
+    if (data[middle] === x) {
+      return true;
+    } else if (data[middle] < x) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
     }
   }
 
-  return null
+  return false;
 }
-console.log(findCat(data));
+
+let sortedData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(binarySearch(sortedData, 1));
