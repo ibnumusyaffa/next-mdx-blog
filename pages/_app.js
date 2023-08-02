b88267c5dcddf88623ed3ba40698320788dc4579
Ibnu Musyaffa <ibnu.musyaffa@gmail.com>
2023-08-03 02:32:07 +0700

    add vercel analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import "@fontsource/roboto/400.css"; 
-import "@fontsource/roboto/500.css"; 
-import "@fontsource/roboto/700.css"; 
-import "@fontsource/roboto/900.css"; 
-import "@fontsource/roboto/400-italic.css"; 
-import "@fontsource/roboto/500-italic.css"; 
-
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/500-italic.css";
 
 import "@fontsource/jetbrains-mono/400.css";
 import "@fontsource/jetbrains-mono/400-italic.css";
-
+import { Analytics } from "@vercel/analytics/react";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <Analytics />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
