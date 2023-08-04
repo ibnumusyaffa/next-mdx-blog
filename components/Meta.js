2f34812a9ba65d4020f787dd2c7e7df84a85814d
Ibnu Musyaffa <ibnu.musyaffa@gmail.com>
2023-08-04 10:23:52 +0700

    update design

--- components/Meta.js
+++ components/Meta.js
@@ -16,22 +16,24 @@ function Meta({
         content="width=device-width, initial-scale=1.0"
       ></meta>
       <meta name="title" content={title} />
-      <meta name="description" content={description} />
+      <meta name="description"   content={description ? description : ""} />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta property="og:description"   content={description ? description : ""} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:image" content={thumbnail} />
       <meta property="og:image:alt" content={title} />
 
-
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
+      <meta
+        property="twitter:description"
+        content={description ? description : ""}
+      />
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:creator" content="@IbnuMusyaffa"></meta>
       <meta property="twitter:image" content={thumbnail} />
